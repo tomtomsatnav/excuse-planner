2875add3a8bd6cded36141feda7fe1af23673ee7
Tom Mitchell <tommitchell@Toms-Mac.broadband>
2023-12-01 16:21:43 +0000

    added event creation pop-up functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,52 @@
 // TODO: Timeslot functions - generate unique id / generate the timeblock with id
 
 // TODO: Event Creation functions - create pop up with user input fields
+function showEventPopup() {
+    const popup = document.createElement('div');
+    popup.classList.add('event-popup');
+    popup.innerHTML = `
+      <h2>Create Event</h2>
+      <label for="eventName">Event Name:</label>
+      <input type="text" id="eventName" required>
+      <label for="eventTime">Event Time:</label>
+      <input type="time" id="eventTime" required>
+      <label for="eventDate">Event Date:</label>
+      <input type="date" id="eventDate" required>
+      <button onclick="saveEvent()">Save Event</button>
+      <button onclick="closePopup()">Cancel</button>
+    `;
+    document.body.appendChild(popup);
+  }
 
-// TODO: Event Creation functions - record and display user input event details on timeslot
+// TODO: Event Creation functions - close popup
+function closePopup() {
+    const popup = document.querySelector('.event-popup');
+    if (popup) {
+        document.body.removeChild(popup);
+    }
+}
 
 // TODO: Event Creation functions - save event details to local storage with specific timeslot and date
+function saveEvent() {
+    const eventName = document.getElementById('eventName').ariaValueMax;
+    const eventDate = document.getElementById('eventDate').ariaValueMax;
+    const eventTime = document.getElementById('eventTime').ariaValueMax;
 
+    const eventDetails = {
+        name: eventName,
+        date: eventDate,
+        time:eventTime
+    };
+
+    const eventDetailsJSON = JSON.stringify(eventDetails)
+
+    localStorage.setItem('eventDetails', eventDetailsJSON)
+};
+
+// TODO: Event Creation functions - record and display user input event details on timeslot
+function updateScheduleDisplay() {
+    
+}
 // TODO: Event block functions - button to modify event function
 
 // TODO: Event block functions - generate excuse function
